9bba6508fbd31fc0365fc40c6d62208b11f8a2b1
Sammiee4real <adebsholey4real@gmail.com>
2024-02-17 07:27:21 +0100

    added endpoints for login, logout, register

--- app/Http/Controllers/Api/RiderAuthController.php
+++ app/Http/Controllers/Api/RiderAuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Models\Rider;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
@@ -22,7 +24,7 @@ class RiderAuthController extends Controller
         if (! $user || ! Hash::check($request->password, $user->password)) {
             return   response()->json([
                 'status' => 'failed',
-                'message' => 'authentication failed',
+                'message' => 'Wrong login credentials',
                 'user' => $user 
             ]);
     
@@ -36,5 +38,89 @@ class RiderAuthController extends Controller
             'user' => $user 
         ]);
 
+    }
+
+    public function register(Request $request){
+        //validate
+        $request->validate([
+            "name"=> "required",
+            "email"=> "required",
+            "phone_number"=> "required",
+            "gender"=> "required",
+            "nationality" => "required",
+            "address"=> "required",
+            "community"=> "required",
+            "password"=> "required",
+        ]);
+
+        $password =  Hash::make($request->password);
+     
+        $rider = Rider::create([
+            "name"=> $request->name,
+            "email"=> $request->email,
+            "phone_number"=> $request->phone_number,
+            "gender"=> $request->gender,
+            "nationality" => $request->nationality,
+            "address"=> $request->address,
+            "community"=> $request->community,
+            "status"=>1,
+        ]);
+
+        $user = User::create([
+            'name' => $rider->name,
+            'email' => $rider->email,
+            'userable_id' => $rider->id,
+            'userable_type' => Rider::class,
+            'partner_id' =>  1, //this will be trekk partner ID by defaul
+            'password' => $password, // password
+            'remember_token' => Str::random(10),
+        ]);
+     
+        if (! $user || !$rider) {
+            return   response()->json([
+                'status' => 'error',
+                'message' => 'Registration was not successful',
+                'user' => $user 
+            ]);
+    
+        }
+     
+        $user->token =  $user->createToken($request->email)->plainTextToken;
+        
+        return   response()->json([
+            'status' => 'success',
+            'message' => 'Rider successfully registered',
+            'user' => $user 
+        ]);
+
+    }
+
+    public function logout(Request $request){
+        $request->validate([
+            'token' => 'required|string',
+            'user_id' => 'required',
+        ]);
+
+        try{
+            $token_id  = $request->token;
+            $user = User::firstWhere('id',$request->user_id);
+            
+            //delete
+            // $user->tokens()->where('id', $token_id)->delete();
+            $user->tokens()->delete();
+                
+            return   response()->json([
+                'status' => 'success',
+                'message' => 'logged out',
+                'user' => $user
+            ]);
+
+        }catch(\Exception $ex){
+            return response()->json([
+                'status' => 'error',
+                'message' => $ex->getMessage()
+            ]);
+        }
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\Api\RiderAuthController;
+use App\Http\Controllers\Api\Riders\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,11 +20,14 @@ use App\Http\Controllers\Api\RiderAuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/', function (Request $request) {
+// return $request->user();
+Route::post('/rider/dashboard',[DashboardController::class,'index'])->name('rider.dashboard')->middleware('auth:sanctum');
+// });
 
 
 
 //MOBILE APP - CLEAN UP LATER
-Route::post('/rider/login',[RiderAuthController::class,'login'])->name('auth.rider');
+Route::post('/rider/login',[RiderAuthController::class,'login'])->name('auth.rider.login');
+Route::post('/rider/logout',[RiderAuthController::class,'logout'])->name('auth.rider.logout');
+Route::post('/rider/register',[RiderAuthController::class,'register'])->name('auth.rider.register');
